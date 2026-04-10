74d271c413ea7d0aa080fd18669eb95a6a6835e8
jaime-tovar <misterjto591@gmail.com>
2026-04-09 19:39:38 -0500

    feat: se agrega db.commit a main de seed_db

--- seed_db.py
+++ seed_db.py
@@ -106,7 +106,7 @@ def seed_categorias(db, admin):
 
     for cat in CATEGORIAS_INICIALES:
         existe = db.query(Categoria).filter_by(descripcion=cat["descripcion"]).first()
-        print(f"Usuario: {admin.id_usuario}")
+
         if not existe:
             nueva_categoria = Categoria(
                 descripcion=cat["descripcion"],
@@ -227,6 +227,7 @@ def main():
             seed_mesas(db, admin)
             print("Sembrando métodos de pago...")
             seed_metodos_pago(db, admin)
+            db.commit()
             print("Seed completado.")
         finally:
             db.close()
